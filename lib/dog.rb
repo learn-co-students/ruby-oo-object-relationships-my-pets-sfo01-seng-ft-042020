3c97c679b24d05a6de321eb8becabd2ff61a53d3
finalbout <johntjr.22@gmail.com>
2020-04-24 14:08:24 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
+
+    attr_writer :owner, :mood
+    attr_reader :name, :owner, :mood
+
+    @@all = [ ]
+
+  def initialize(name,owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
+
+ 
+
+
   # code goes here
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,76 @@
+require 'pry'
+
 class Owner
+
+    attr_reader :name, :species
+
+  @@all = [ ]
+
+    def initialize(name)
+      @name = name
+      @species = "human"
+      @@all << self
+    
+    end
+
+    def say_species
+      p "I am a #{species}."
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.count
+      self.all.count
+    end
+
+    def self.reset_all
+      self.all.clear
+    end
+
+    def cats
+      Cat.all.select {|cats| cats.owner == self}
+    end
+
+    def dogs
+      Dog.all.select {|dogs| dogs.owner == self}
+    end
+
+    def buy_cat(name)
+      Cat.new(name,self)
+    end
+
+    def buy_dog(name)
+      Dog.new(name,self)
+    end
+
+    def walk_dogs
+      self.dogs.each {|change| change.mood = "happy"}
+    end
+
+    def feed_cats
+      self.cats.each {|change| change.mood = "happy"}
+    end
+
+    def sell_pets
+      self.cats.each do |change|
+        change.mood = "nervous"
+        change.owner = nil
+    end
+
+      self.dogs.each do |change|
+        change.mood ="nervous"
+        change.owner = nil
+      end
+
+      self.dogs.clear
+      self.cats.clear
+  end
+
+    def list_pets
+       "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    end
+
   # code goes here
 end
