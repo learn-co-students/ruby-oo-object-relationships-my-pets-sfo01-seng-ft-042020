83eaa63b2c3211655ad9a6b4c3a1b397c8d2633a
aa1b02ab7492d696da6e8ac540ffa187ba623c88e15238d8d026f79bc5d39b3c <darrickpang@DESKTOP-02GI1R6.localdomain>
2020-04-27 15:24:16 -0700

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
 class Owner
-  # code goes here
+  require 'pry'
+  attr_reader :name, :species
+  @@all = []
+  def initialize(name)
+    @name = name
+    @cats = []
+    @dogs = []
+    @@all << self
+  end 
+  def species(species = "human")
+    @species = species
+  end 
+  def say_species
+    return "I am a human."
+  end 
+  def self.all
+    @@all
+  end 
+  def self.count 
+    return @@all.length
+  end 
+  def self.reset_all
+    @@all = []
+    return @@all
+  end 
+  def att_cats(cats)
+    @cats << cats
+    cats.owner = self
+  end 
+  def cats
+    @cats 
+  end 
+  def att_dogs(dogs)
+    @dogs << dogs 
+    dogs.owner = self
+  end 
+  def dogs
+    @dogs 
+  end 
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end 
+
+  def buy_dog(name)
+    Dog.new(name,self)
+  end 
+  def walk_dogs
+    @dogs.each {|dog| dog.mood = "happy"}
+  end 
+
+  def feed_cats 
+    @cats.each {|cat| cat.mood = "happy"}
+  end
+  def sell_pets
+    @dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil 
+      @dogs.delete(dog)
+    end 
+    @cats.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+      @cats.delete(cat)
+    end 
+  end 
+  def list_pets
+    return "I have #{@dogs.count} dog(s), and #{@cats.count} cat(s)."
+  end 
 end
